c8b9b5b9ca0968291d5adc98f468e77753e46b31
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2019-12-07 19:24:29 -0600

    CSC 20191207: Implementando patron de repositorios y logger

---
+++ NetCoreRepositoryPattern/NetCoreRepositoryPattern/Services/EventRepositiry.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using repositorypattern.Context;
+using repositorypattern.Models;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreRepositoryPattern.Services
+{
+    public class EventRepositiry : IEventRepository
+    {
+        private readonly EventContext _eventContext;
+        private readonly ILogger<EventRepositiry> _logger;
+
+
+        public EventRepositiry(EventContext eventContext, ILogger<EventRepositiry> logger)
+        {
+            _eventContext = eventContext;
+            _logger = logger;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comedian> GetComedian(int comedianId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comedian[]> GetComedians()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comedian[]> GetComediansByEvent(int eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetEvent(bool includeGigs = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event[]> GetEventArgs(bool includeGigs = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event[]> GetEventsByDate(DateTime date, bool includeGigs = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Gig> GetGigByEvent(int eventId, bool includeComedians = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Gig[]> GetGigsByEvent(int eventId, bool includeComedians = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Gig[]> GetGigsByVenue(int venueid, bool includeComedians = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetCoreRepositoryPattern/NetCoreRepositoryPattern/Services/IEventRepository.cs
@@ -0,0 +1,32 @@
+﻿using repositorypattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreRepositoryPattern.Services
+{
+    public interface IEventRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        Task<bool> Save();
+
+        //events;
+        Task<Event[]> GetEventArgs(bool includeGigs = false);
+        Task<Event> GetEvent(bool includeGigs = false);
+        Task<Event[]> GetEventsByDate(DateTime date, bool includeGigs = false);
+
+        //gigs
+        Task<Gig[]> GetGigsByEvent(int eventId, bool includeComedians = false);
+        Task<Gig> GetGigByEvent(int eventId, bool includeComedians = false);
+        Task<Gig[]> GetGigsByVenue(int venueid, bool includeComedians = false);
+
+        //comedians
+        Task<Comedian[]> GetComedians();
+        Task<Comedian[]> GetComediansByEvent(int eventId);
+        Task<Comedian> GetComedian(int comedianId);
+
+    }
+}
